3f23ba9d36ada061fcdc0e5831a62f5efd1e8dce
doxas <eizanco@gmail.com>
2020-01-08 20:27:17 +0900

    update unit test of geometry

--- __tests__/Geometry.test.js
+++ __tests__/Geometry.test.js
@@ -69,5 +69,37 @@ describe('static method in Geometry', () => {
         const geometry = Geometry.cone(split, rad, height, color);
         expect(geometry).hasOwnProp();
     });
+    test('Geometry.cylinder return value', () => {
+        const split = 8;
+        const topRad = 1.0;
+        const bottomRad = 1.0;
+        const height = 1.0;
+        const color = [0.1, 0.2, 0.3, 1.0];
+        const geometry = Geometry.cylinder(split, topRad, bottomRad, height, color);
+        expect(geometry).hasOwnProp();
+    });
+    test('Geometry.sphere return value', () => {
+        const row = 8;
+        const column = 8;
+        const rad = 1.0;
+        const color = [0.1, 0.2, 0.3, 1.0];
+        const geometry = Geometry.sphere(row, column, rad, color);
+        expect(geometry).hasOwnProp();
+    });
+    test('Geometry.torus return value', () => {
+        const row = 8;
+        const column = 8;
+        const irad = 1.0;
+        const orad = 1.0;
+        const color = [0.1, 0.2, 0.3, 1.0];
+        const geometry = Geometry.torus(row, column, irad, orad, color);
+        expect(geometry).hasOwnProp();
+    });
+    test('Geometry.icosahedron return value', () => {
+        const rad = 1.0;
+        const color = [0.1, 0.2, 0.3, 1.0];
+        const geometry = Geometry.icosahedron(rad, color);
+        expect(geometry).hasOwnProp();
+    });
 });
 
